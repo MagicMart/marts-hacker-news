7ddb412e23d3b73596870cedc89ca69cf84184e8
Martin Tudor <martcan27@gmail.com>
2019-12-14 21:45:48 +0000

    add react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
 // @flow
 
 import React from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
 import Posts from "./Posts";
 
 function App() {
     return (
-        <div>
-            <h1>Hacker News Clone</h1>
-            <Posts type="top" />
-        </div>
+        <Router>
+            <div>
+                <h1>Hacker News Clone</h1>
+                <ul>
+                    <li>
+                        <Link to="/">Top</Link>
+                    </li>
+                    <li>
+                        <Link to="/new">New</Link>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route exact path="/" render={() => <Posts type="top" />} />
+                    <Route path="/new" render={() => <Posts type="new" />} />
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
